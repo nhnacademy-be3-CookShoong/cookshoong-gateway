2c0d09c6776096c0216255a2e319a866dd6f2080
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-07-04 13:43:02 +0900

    refactor: 💡 수정 properties파일 분리
    properties 파일 분리하여 관리

--- src/main/java/store/cookshoong/www/cookshoonggateway/CookshoongGatewayApplication.java
+++ src/main/java/store/cookshoong/www/cookshoonggateway/CookshoongGatewayApplication.java
@@ -2,8 +2,16 @@ package store.cookshoong.www.cookshoonggateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+/**
+ * gateway main.
+ *
+ * @author seungyeon
+ * @since 2023/07/04
+ */
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CookshoongGatewayApplication {
 
     public static void main(String[] args) {
